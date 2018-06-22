9e834b6a43c78d0c1de3c4d34502721a8f6d8fe6
arielbk <buchwald.kearney@gmail.com>
2018-06-22 21:40:46 +0200

    all functionality now possible with keys (add new photo)

--- image-gallery-react/src/App.js
+++ image-gallery-react/src/App.js
@@ -54,7 +54,8 @@ class App extends Component {
         { uuid: uuidv4(), photoUrl: 'https://source.unsplash.com/random/1600x1201', description: 'and another short description', selected: false },
         { uuid: uuidv4(), photoUrl: 'https://source.unsplash.com/random/1600x1202', description: 'and another short description', selected: false },
         { uuid: uuidv4(), photoUrl: 'https://source.unsplash.com/random/1600x1203', description: 'and another short description', selected: false },
-      ]
+      ],
+      addPhotoSelected: false,
     }
 
     this.handleSelectImage = this.handleSelectImage.bind(this);
@@ -65,15 +66,28 @@ class App extends Component {
   }
 
   componentDidMount() {
-    document.addEventListener('keydown', this.handleKeyPress)
+    document.addEventListener('keydown', this.handleKeyPress);
   }
 
   componentDidUpdate() {
     const sidebar = document.querySelector('.sidebar');
+    const addPhotobox = document.querySelector('.img-add');
+
     sidebar.style.height = `${(this.state.photos.length+1) * 186}px`;
+
     const selected = this.state.photos.filter(photo => photo.selected);
     const index = this.state.photos.indexOf(selected[0]);
-    sidebar.style.marginTop = `-${index * 170}px`;
+    if (!this.state.addPhotoSelected) {
+      sidebar.style.marginTop = `-${index * 138}px`;
+      addPhotobox.classList.remove('img-add-selected');
+    } else {
+      // move sidebar up for selecting add photo
+      sidebar.style.marginTop = `-${index * 138 + 138}px`;
+      // provide styles so user knows it is selected
+      
+      addPhotobox.classList.add('img-add-selected');
+      // add listener for pressing enter while it is selected
+    }
   }
 
   handleSelectImage(newImage) {
@@ -88,26 +102,40 @@ class App extends Component {
   }
 
   handleKeyPress(e) {
-    const sideBar = document.querySelector('.sidebar');
-    if (e.key > 0 && e.key < 9) {
-      if (e.key > this.state.photos.length) return;
-      sideBar.style.marginTop = `${(Number(e.key)+1) * -186}px`;
-      console.log(sideBar.style.height);
-      this.handleSelectImage(this.state.photos[e.key-1]);
-      return;
-    }
 
-    // otherwise figure out the index of the current image
+    // // THIS IS FOR NUMBER KEYS TO SELECT A PHOTO - BUT ITS NOT REALLY NEEDED (FOR NOW)
+    // if (e.key > 0 && e.key < 9) {
+    //   if (e.key > this.state.photos.length) return;
+    //   sideBar.style.marginTop = `${(Number(e.key)+1) * -186}px`;
+    //   console.log(sideBar.style.height);
+    //   this.handleSelectImage(this.state.photos[e.key-1]);
+    //   return;
+    // }
+
+    // figure out the index of the current image
     const current = this.state.photos.filter(photo => photo.selected === true);
     const currentIndex = this.state.photos.indexOf(current[0]);
 
     // now for the up and down arrow
-    if (e.key === 'ArrowUp' && currentIndex > 0) {
+    if (e.key === 'ArrowUp' && currentIndex === this.state.photos.length - 1 && this.state.addPhotoSelected) {
+      // moving selection away from 'add new photo'
+      this.setState({ addPhotoSelected: false });
+    } else if (e.key === 'ArrowUp' && currentIndex > 0) {
       this.handleSelectImage(this.state.photos[currentIndex-1]);
     } else if (e.key === 'ArrowDown' && currentIndex < this.state.photos.length - 1) {
       this.handleSelectImage(this.state.photos[currentIndex+1]);
+    } else if (e.key === 'ArrowDown' && currentIndex === this.state.photos.length - 1 && !this.state.addPhotoSelected) {
+      // if we are on the last photo and press the down arrow, 'add new button' should be selected
+      this.setState({ addPhotoSelected: true });
     }
-  
+
+    // when enter is pressed on add new photo... add a new photo
+    if (e.key === 'Enter' && this.state.addPhotoSelected) {
+      this.handleAddPhoto();
+      this.setState({ addPhotoSelected: false });
+    } else if (e.key === 'Enter') {
+      this.handleSelectImage(this.state.photos[currentIndex])
+    }
   }
 
   handleAddPhoto() {
@@ -115,7 +143,7 @@ class App extends Component {
     const uuidv4 = require('uuid/v4');
     const newPhoto = { uuid: uuidv4(), photoUrl: `https://source.unsplash.com/random/1600x${1200+this.randomImageCounter}`, description: 'a short description', selected: false }
     photos.push(newPhoto);
-    this.setState({ photos });
+    this.setState({ photos }, () => this.handleSelectImage(this.state.photos[this.state.photos.length-1]));
     this.randomImageCounter++;
   }
 
@@ -125,7 +153,6 @@ class App extends Component {
         <DisplayImage 
           photo={this.state.photos.filter(photo => photo.selected)}
         />
-        <p>Use your keyboard up and down arrow keys to navigate the photos</p>
         <div className="sidebar-container">
           <div className="sidebar">
           <ImageThumbs 
