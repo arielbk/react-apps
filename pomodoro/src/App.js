38915b2e44f0bb2f7d8e1fc798d359c57b02e5ee
arielbk <buchwald.kearney@gmail.com>
2018-06-30 13:38:00 +0200

    fresh start for pomodoro timer

---
+++ pomodoro/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <h1>React Pomodoro Timer Coming Soon!</h1>
+    );
+  }
+}
+
+export default App;
