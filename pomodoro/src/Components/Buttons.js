dbc53d642cecc033be82ba172fcc569e4de767aa
arielbk <buchwald.kearney@gmail.com>
2018-07-02 22:52:18 +0200

    Pomodoro - node changes and icons added

--- pomodoro/src/Components/Buttons.js
+++ pomodoro/src/Components/Buttons.js
@@ -4,7 +4,14 @@ class Buttons extends Component {
   render() {
     return (
     <div className="buttons">
-      <a className="countdown" onClick={this.props.onPlayPause}>{this.props.content}</a>
+      <a className="countdown" onClick={this.props.onPlayPause}>
+        {this.props.content === 'Work' ||
+          this.props.content === 'Break' ||
+          this.props.content === 'Continue'
+            ? <i className="fas fa-play" /> 
+            : <i className="fas fa-pause" />}
+        {this.props.content}
+      </a>
       <a className="reset" onClick={this.props.onReset}><i className="fas fa-sync-alt"></i> Reset</a>
     </div>
     );
